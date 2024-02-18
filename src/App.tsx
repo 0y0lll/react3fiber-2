80d9a0fd6b2231ec0d88b26e055c6e8480fedd7f
oyo <bemyangel2@gmail.com>
2024-02-18 11:06:00 +0900

    feat: controls, helpers, leva

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import { Canvas } from "@react-three/fiber"
 import "./App.css"
 import { ThreeElement } from "./ThreeElement"
+import { OrbitControls } from "@react-three/drei"
+import { useControls } from "leva"
 
 function App() {
+	const color = useControls({
+		value: "#9ec7ce",
+	})
+
+	const grid = useControls({
+		segment: { value: 10, min: 2, max: 100, step: 1 },
+	})
+
 	return (
-		<Canvas>
+		<Canvas
+			camera={{
+				near: 1,
+				far: 100, // 멀리 있는 것 까지 렌더링 할 수치
+				fov: 75,
+				position: [5, 5, 0],
+			}}
+		>
+			<color attach="background" args={[color.value]} />
+			<OrbitControls />
+			<axesHelper args={[5]} />
+			<gridHelper args={[20, grid.segment]} />
 			<ThreeElement />
 		</Canvas>
 	)
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,11 +1,25 @@
+import { useFrame, useThree } from "@react-three/fiber"
+import { useRef } from "react"
+import { Mesh } from "three"
 import { degToRad } from "three/src/math/MathUtils.js"
+import { useControls } from "leva"
 
 export const ThreeElement = () => {
+	const { scene } = useThree()
+	const ref = useRef<Mesh>(null)
+
+	// 오브젝트를 움직이는 frame
+	useFrame((delta) => {})
+
+	const box = useControls({
+		rotation: { value: 0, min: -360, max: 360, step: 1 },
+	})
+
 	return (
 		<>
 			<directionalLight position={[5, 5, 5]} />
 
-			<mesh rotation={[degToRad(30), degToRad(45), 0]}>
+			<mesh ref={ref} rotation={[degToRad(45), box.rotation, 0]}>
 				<boxGeometry />
 				<meshStandardMaterial color="green" />
 			</mesh>
